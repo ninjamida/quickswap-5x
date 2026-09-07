b9be5cdff92a7438f942c4fce76b9402f89bb49a
Namida Verasche <namida.verasche@gmail.com>
2026-09-08 01:42:14 +1200

    Speed calibration test, and some associated readme notes.

--- quickswap.py
+++ quickswap.py
@@ -39,11 +39,15 @@ class QuickSwap:
 
         self.printer.register_event_handler("klippy:ready", self._handle_ready)
 
+        # Filament change commands
         self.gcode.register_command('_QS_CHANGE_FILAMENT', self.cmd_QS_CHANGE_FILAMENT)
         self.gcode.register_command('_QS_PURGE_OLD_FILAMENT', self.cmd_QS_PURGE_OLD_FILAMENT)
         self.gcode.register_command('_QS_WAIT_IFS_IDLE', self.cmd_QS_WAIT_IFS_IDLE)
         self.gcode.register_command('_QS_GENERATE_TEST', self.cmd_QS_GENERATE_TEST)
         self.gcode.register_command('_QS_IFS_ASYNC_COMMAND', self.cmd_QS_IFS_ASYNC_COMMAND)
+
+        # Calibration commands
+        self.gcode.register_command('_QS_IFS_CALIBRATION_SPEED', self.cmd_QS_IFS_CALIBRATION_SPEED)
 
         self.ifs_flag_time = time.monotonic() - self.ifs_flag_delay
 
@@ -124,6 +128,10 @@ class QuickSwap:
             new_name,
             original_handler
         )
+
+# =============================================================================
+# FILAMENT CHANGE FUNCTIONS
+# =============================================================================
 
     def cmd_QS_GENERATE_TEST(self, gcmd):
         channel = gcmd.get_int('CHANNEL', 0)
@@ -343,7 +351,7 @@ class QuickSwap:
             self._qs_unload_old_filament(old_channel, old_filament_info, cmds)
 
         self._qs_load_new_filament(old_filament_info, target_channel, new_filament_info, skip_unload, cmds)
-                
+
         if skip_unload:
             cmds += ['SET_FAN_SPEED FAN=fanM106 SPEED=0']
             cmds += [f"G1 E{new_filament_info['filament_drop_length']} F{new_filament_info['filament_extruder_speed']}"]
@@ -581,7 +589,7 @@ class QuickSwap:
         insert_length = self.insert_base_distance
         if not skip_unload:
             insert_length += old_filament_info['filament_unload_before_cutting']
-        
+
         cmds += [f"G1 E{insert_length} F{new_filament_info['filament_extruder_speed']}"]
         cmds += [f"IFS_F10 PRUTOK={new_channel} LEN={round(insert_length)} SPEED={int(new_filament_info['filament_extruder_speed'] * speed_factor)} SLEEP=1"]
         cmds += ["M400"]
@@ -643,6 +651,134 @@ class QuickSwap:
                 json.dump(mapping, f)
 
         return new_channel
+
+# =============================================================================
+# CALIBRATION FUNCTIONS
+# =============================================================================
+
+    def _qs_move_to_sensor(self, channel, speed, tolerance, tube_flex, text=None):
+        if text:
+            self.gcode.respond_raw(text)
+            
+        while self.zmod_ifs.get_extruder_sensor():
+            self.gcode.run_script_from_command(f"IFS_F11 PRUTOK={channel} LEN=1 SPEED={speed} WAIT=1\nM400")
+            time.sleep(0.6)
+            
+        if tube_flex > 0:
+            self.gcode.run_script_from_command(f"IFS_F10 PRUTOK={channel} LEN={tube_flex} SPEED={speed} WAIT=1\nM400")
+            
+        return self._qs_move_to_sensor_forward(channel, speed, tolerance)
+        
+    def _qs_move_to_sensor_forward(self, channel, speed, tolerance, text=None):
+        if text:
+            self.gcode.respond_raw(text)
+            
+        reinsert_steps = 0
+        while not self.zmod_ifs.get_extruder_sensor():
+            self.gcode.run_script_from_command(f"IFS_F10 PRUTOK={channel} LEN=1 SPEED={speed} WAIT=1\nM400")
+            time.sleep(0.6)
+            reinsert_steps += 1
+            if reinsert_steps > tolerance:
+                break
+
+        return reinsert_steps
+
+    def cmd_QS_IFS_CALIBRATION_SPEED(self, gcmd):
+        self.gcode.run_script_from_command('RESPOND TYPE=command MSG="action:prompt_end"')
+        
+        channel = gcmd.get_int('CHANNEL')
+        min_speed = gcmd.get_int('MIN_SPEED')
+        max_speed = gcmd.get_int('MAX_SPEED')
+        speed_step = gcmd.get_int('SPEED_STEP')
+        test_length = gcmd.get_int('TEST_LENGTH')
+        tolerance = gcmd.get_int('TOLERANCE')
+        tube_flex = gcmd.get_int('TUBE_FLEX')
+
+        result_insert = max_speed + 1
+        result_withdraw = max_speed + 1
+
+        slow_speed = min(min_speed, 150)
+
+        filament_info = self.zmod_ifs.get_prutok_config(channel)
+
+        if self.zmod_ifs.get_extruder_sensor():
+            self.gcode.respond_raw('FAILED: Please unload extruder and try again.')
+            return
+        if not self.zmod_ifs.ifs_data.get_port(channel):
+            self.gcode.respond_raw(f'FAILED: Please load IFS channel {channel} and try again.')
+            return
+
+        self.gcode.respond_raw('Inserting filament')
+        self.gcode.run_script_from_command(f"IFS_F24 PRUTOK={channel}")
+        self.gcode.run_script_from_command(f"IFS_F10 PRUTOK={channel} LEN={filament_info['filament_tube_length']} SPEED={min_speed} CHECK=1\nM400")
+
+        if not self.zmod_ifs.get_extruder_sensor():
+            self.gcode.respond_raw('FAILED: Filament not detected in print head after insertion.')
+            return
+
+        if tube_flex >= 0:
+            if self._qs_move_to_sensor(channel, slow_speed, tolerance, tube_flex, 'Verifying filament positioning') > tolerance:
+                self.gcode.respond_raw(f"FAILED: More than {reinsert_steps}mm reinsertion was needed. Try adjusting tube flex length or increasing tolerance.")
+                return
+        else:
+            max_autodetect_steps = 15
+            move_steps = self._qs_move_to_sensor(channel, slow_speed, max_autodetect_steps, -1, 'Determining tube flex length')
+            if move_steps > max_autodetect_steps:
+                self.gcode.respond_raw(f"FAILED: Filament not detected after {max_autodetect_steps} steps.")
+                return
+            tube_flex = move_steps - 1
+            self.gcode.respond_raw(f"Tube flex length detected: {tube_flex}mm")
+            
+
+        this_speed = min_speed
+        last_speed = min_speed
+        while True: # break condition at end of loop
+            redo_iteration = False
+
+            self.gcode.respond_raw(f'Running test: Withdraw speed {min(this_speed, result_withdraw)}, insert speed {min(this_speed, result_insert)}')
+            self.gcode.run_script_from_command(f"IFS_F11 PRUTOK={channel} LEN={test_length} SPEED={min(this_speed, result_withdraw)} WAIT=1\nM400")
+            self.gcode.run_script_from_command(f"IFS_F10 PRUTOK={channel} LEN={test_length - tolerance} SPEED={min(this_speed, result_insert)} WAIT=1\nM400")
+            time.sleep(0.6)
+
+            if self.zmod_ifs.get_extruder_sensor():
+                result_withdraw = last_speed
+                self._qs_move_to_sensor(channel, slow_speed, tolerance, tube_flex, 'Repositioning filament')
+                redo_iteration = True
+            else:
+                insert_step_count = self._qs_move_to_sensor_forward(channel, slow_speed, tolerance * 2)
+
+                if not self.zmod_ifs.get_extruder_sensor():
+                    result_insert = last_speed
+                    redo_iteration = True
+                    max_insert_steps = 15
+                    self.gcode.run_script_from_command(f"IFS_F10 PRUTOK={channel} LEN={filament_info['filament_tube_length']} SPEED={min_speed} CHECK=1\nM400")
+                    if self._qs_move_to_sensor_forward(channel, slow_speed, max_insert_steps, 'Repositioning filament') > max_insert_steps:
+                        self.gcode.respond_raw("FAILED: Unable to reposition filament position")
+                        return
+                elif insert_step_count > tolerance * 2: # In case the "first over the limit" iteration activated the sensor
+                    result_insert = last_speed
+                    redo_iteration = True
+
+            if result_withdraw <= max_speed and result_insert <= max_speed:
+                break
+
+            if redo_iteration:
+                continue
+
+            last_speed = this_speed
+            if this_speed < max_speed:
+                this_speed = min(this_speed + speed_step, max_speed)
+            else:
+                break
+
+        self.gcode.respond_raw(f'Test result: Withdraw speed {min(max_speed, result_withdraw)}, insert speed {min(max_speed, result_insert)}')
+        if result_withdraw > max_speed and result_insert > max_speed:
+            self.gcode.respond_raw('Both directions succeeded at maximum test speed')
+        elif result_withdraw > max_speed:
+            self.gcode.respond_raw('Withdrawal succeeded at maximum test speed')
+        elif result_insert > max_speed:
+            self.gcode.respond_raw('Insert succeeded at maximum test speed')
+
 
 def load_config(config):
     return QuickSwap(config)
